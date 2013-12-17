f6ae7c4a163131ae46488134171fab3e62a3e1dc
Márcio Martins <marciomartins.it@gmail.com>
2013-12-17 15:58:34 +0000

    #12 Update Model Add User Model

--- Web/EpicWareWeb/EpicWareWeb/Models/User.cs
+++ Web/EpicWareWeb/EpicWareWeb/Models/User.cs
@@ -12,6 +12,10 @@ namespace EpicWareWeb.Models
         public List<Connection> listConnections { get; set; }
         public Language language { get; set; }
         public List<ScoreUser> scores { get; set; }
+        public List<Notification> notifications { get; set; }
+        public List<Mission> missions { get; set; }
+        public Mood mood { get; set; }
+        public Profile userProfile { get; set; }
         public bool active { get; set; }
     }
 }
