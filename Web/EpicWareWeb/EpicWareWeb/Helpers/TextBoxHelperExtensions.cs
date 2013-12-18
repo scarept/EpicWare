4180608eda77ad2c7707cee9083d1925852c32e1
Ricardo Brandão <ricardombrandao@gmail.com>
2013-12-18 12:03:06 +0000

    #13 Calendar added to create user on birthday

---
+++ Web/EpicWareWeb/EpicWareWeb/Helpers/TextBoxHelperExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace System.Web.Mvc.Html 
+{
+    public static class TextBoxHelperExtensions
+    {
+        /// <summary>  
+        /// Retorna o elemento input do tipo text com calendário  
+        /// </summary>  
+        public static MvcHtmlString TextBoxDatePickerFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
+          where TModel : class
+        {
+            /* 
+            Extrai metadados da expressão lambda passada (ex: model => model. 
+            DataNascimento), onde precisamos do nome e valor da propriedade 
+            */
+            ModelMetadata metadados = ModelMetadata.FromLambdaExpression(expression, htmlHelper.ViewData);
+
+            /* 
+            Valor obtido no metadados para que possamos  
+            formata-los, senão mostraria a hora junto com a data. 
+            */
+            DateTime? data = (metadados.Model as DateTime?);
+
+            /* 
+            Chama o helper TextBox do próprio .NET passando o nome da  
+            propriedade, e retornando o valor da data no formato  
+            dd/MM/yyyy e atribuindo a classe "datePicker" no input que  
+            será retornado para a view. 
+            */
+            return htmlHelper.TextBox(
+                metadados.PropertyName,
+                data != null && data != default(DateTime) ? data.Value.ToString("dd/MM/yyyy") : string.Empty,
+                new { @class = "datePicker" });
+        }
+    }  
+}
---
+++ Web/EpicWareWeb/EpicWareWeb/Scripts/inputs-jquery-ui.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    /* 
+    para todos os elementos da classe que vamos chamar de "datePicker", 
+    será aplicado o "componente" de calendário no formato brasileiro. 
+    */
+    $(".datePicker").datepicker({ dateFormat: "dd/mm/yy" });
+});
