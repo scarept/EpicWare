640831ef52e7c11bc26573ca4a7f91ddb5e1d327
Márcio Martins <marciomartins.it@gmail.com>
2013-12-23 19:48:15 +0000

    #13 New Methods: // Return if user autenticated have requested "friend-request" for user parameter public bool friendRequested(User user) // Return if user autenticated have friend requested by user parameter public bool friendRequestReceived(User user)  // List of No Coomon Friends between User Autenticated and User parameter public List<User> noCommonFriends(User user)

--- Web/EpicWareWeb/EpicWareWeb/Controllers/ConnectionController.cs
+++ Web/EpicWareWeb/EpicWareWeb/Controllers/ConnectionController.cs
@@ -14,11 +14,6 @@ namespace EpicWareWeb.Controllers
         //
         // GET: /Connection/
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         // Return if user is friend of user autenticated
         [Authorize]
         public bool isFriends(User user)
@@ -37,12 +32,62 @@ namespace EpicWareWeb.Controllers
             return flag;
 
         }
+        
+        // Return if user autenticated have requested "friend-request" for user parameter
+        [Authorize]
+        public bool friendRequested(User user)
+        {
+            User userAuth = new UserController().UserAutenticated();
+            var fR = from d in db.friendRequests where d.user1 == userAuth && d.user2 == user select d;
+            FriendRequest tmp = fR.ToList().ElementAt(0);
+            if (tmp == null)
+                return false;
+            else
+                return true;
+        }
+        
+        // Return if user autenticated have friend requested by user parameter
+        [Authorize]
+        public bool friendRequestReceived(User user)
+        {
+            User userAuth = new UserController().UserAutenticated();
+            var fR = from d in db.friendRequests where d.user2 == userAuth && d.user1 == user select d;
+            FriendRequest tmp = fR.ToList().ElementAt(0);
+            if (tmp == null)
+                return false;
+            else
+                return true;
+        }
 
+        // List of Friends Requested Pending of user autenticated
+        [Authorize]
         public List<FriendRequest> friendsRequestedPending()
         {
             User userAuth = new UserController().UserAutenticated();
             var fRP = from d in db.friendRequests where d.user2 == userAuth select d;
             return fRP.ToList();
+        }
+
+        // List of No Coomon Friends between User Autenticated and User parameter
+        public List<User> noCommonFriends(User user)
+        {
+            User userAuth = new UserController().UserAutenticated();
+            List<User> returnList = new List<User>();
+            foreach (Connection con in user.listConnections)
+            {
+                bool flag = false;
+                foreach (Connection con2 in userAuth.listConnections)
+                {
+                    if (con.userConnected == con2.userConnected)
+                    {
+                        flag = true;
+                    }
+                    if (flag)
+                        continue;
+                    returnList.Add(con2.userConnected);
+                }
+            }
+            return returnList;
         }
 
     }
