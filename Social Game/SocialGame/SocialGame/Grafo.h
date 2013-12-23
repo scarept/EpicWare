c49cde925f3d14feaf45c873820517bdad5495af
LuisBarbosa <luismendesbarbosa@hotmail.com>
2013-12-23 20:20:29 +0000

    ref #26 #22 Create nodes and conections betwen nodes, no matter the position that is required to connect. Beta camera.

---
+++ Social Game/SocialGame/SocialGame/Grafo.h
@@ -0,0 +1,57 @@
+#ifndef _GRAFO_INCLUDE
+#define _GRAFO_INCLUDE
+
+#define _MAX_NOS_GRAFO 100
+#define _MAX_ARCOS_GRAFO 200
+
+#define NORTE_SUL	0
+#define ESTE_OESTE	1
+#define PLANO		2
+
+typedef struct No{
+	float x, y, z, largura;
+	char * tags;
+	char * estadoHumor;
+}No;
+
+typedef struct Arco{
+	int noi, nof; //substituir depois pelo user id
+	float peso, largura;
+	float forcaLig;
+	char * userIdi;
+	char * userIdf;
+	char * tag;
+	float raio;
+}Arco;
+/*
+typedef struct Ligacao{
+	char * userIdi;
+	char * userIdf;
+	char * forcaRel;
+	int noi;
+	int nof;
+	float raio;
+} Ligacao;
+*/
+extern No nos[];
+extern Arco arcos[];
+//extern Ligacao ligacao[];
+
+extern int numNos, numArcos;
+
+void addNo(No);
+void deleteNo(int);
+void imprimeNo(No);
+void listNos();
+No criaNo(float, float, float);
+
+void addArco(Arco);
+void deleteArco(int);
+void imprimeArco(Arco);
+void listArcos();
+Arco criaArco(int, int, float, float);
+
+void gravaGrafo();
+void leGrafo();
+
+#endif
---
+++ Social Game/SocialGame/SocialGame/LoadImages.h
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <fstream> 
+#include <stdio.h>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <vector>
+#include "SOIL.h"
+
+GLuint carrega_texturas(char * nomeTextura);
