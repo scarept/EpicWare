c132c232f42bf364af2d913a360379584ac1d736
Ricardo Barbosa <1101461@isep.ipp.pt>
2014-01-02 20:55:00 +0000

    ref #11 Added to and redefined HangmanLogic classes and PROLOG predicates for better AI communcation Developed in cooperation with: -1111116 VitorSousa

--- Social Game/MiniGames/HangMan/WCFProlog/WCFProlog/HangmanLogic.cpp
+++ Social Game/MiniGames/HangMan/WCFProlog/WCFProlog/HangmanLogic.cpp
@@ -3,45 +3,117 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <string.h>
 
 HangmanLogic::HangmanLogic(){}
 
-
 HangmanLogic::~HangmanLogic(){}
 
-void HangmanLogic::chooseWord(string topic, string language){
+bool HangmanLogic::isGameWon(string word, char* guessList){
+
+	//Get list length
+	int n = strlen(guessList);
+
+	//Conjure up a list from guesses
+	string guesses = "[";
+	for (int i = 0; i < n; i++){
+		if (i == (n - 1)){
+			guesses.push_back(guessList[i]);
+			guesses.push_back(']');
+		}
+		else{
+			guesses.push_back(guessList[i]); 
+			guesses.push_back(',');
+		}
+	}
+
+	PlTermv av(2);
+
+	//Define the input, Word and GuessList
+	av[0] = PlCompound(word.c_str());
+	av[1] = PlCompound(guesses.c_str());
+
+	//Define the query
+	PlQuery q("gameWon", av);
+
+	//Initialize the return variable
+	bool state = false;
+	
+	//Summom predicate
+	state = q.next_solution();
+
+	return state;
+}
+
+vector<string> HangmanLogic::getTopicsByLanguage(string language){
+
+	//Topic vector to fill and return
+	vector<string> topics;
+
+	PlTermv av(2);
+
+	//Define input, the language
+	av[0] = PlCompound(language.c_str());
+
+	//Define the query
+	PlQuery q("getTopicsByLanguage", av);
+	
+	while (q.next_solution()){
+		
+		//Define string to be tokenized and it's delimiters
+		char * pch = strtok((char*)av[1], " ,'[]");
+
+		while (pch != NULL)
+		{
+			//Add topic to vector
+			topics.push_back((string) pch);
+
+			pch = strtok(NULL, " ,'[]");
+		}
+	}
+
+	return topics;
+}
+
+string HangmanLogic::chooseWord(string topic, string language){
 
 	PlTermv av(3);
 	
+	//Define the input variables
 	av[0] = topic.c_str();
 	av[1] = language.c_str();
 
+	//Define the query
 	PlQuery q("getWordByTopicAndLanguage", av);
+
+	string word;
+
 	while (q.next_solution())
-		cout << (char *)av[2] << endl;
+		word = (string)(char *)av[2];
+
+	return word;
+}
+
+char* HangmanLogic::processWideChar(WCHAR* wc){
+
+	int size;
+	char DefChar = ' ';
+
+	size = wcslen(wc);
+	char *text = (char *)malloc(size);
+	DefChar = ' ';
+	WideCharToMultiByte(CP_ACP, 0, wc, -1, text, 260, &DefChar, NULL);
+
+	return text;
 }
 
 void HangmanLogic::assertData(vector<Word> words){
 
 	for each (Word w in words){
 
-		int size;
-		char DefChar = ' ';
-
-		size = wcslen(w.text);
-		char *text = (char *)malloc(size);
-		DefChar = ' ';
-		WideCharToMultiByte(CP_ACP, 0, w.text, -1, text, 260, &DefChar, NULL);
-
-		size = wcslen(w.topic);
-		char *topic = (char *)malloc(size);
-		DefChar = ' ';
-		WideCharToMultiByte(CP_ACP, 0, w.topic, -1, topic, 260, &DefChar, NULL);
-
-		size = wcslen(w.language);
-		char *language = (char *)malloc(size);
-		DefChar = ' ';
-		WideCharToMultiByte(CP_ACP, 0, w.language, -1, language, 260, &DefChar, NULL);
+		char *text = processWideChar(w.text);
+		char *topic = processWideChar(w.topic);
+		char *language = processWideChar(w.language);
 
 		joinConnection(text, topic, language);
 	}
@@ -51,8 +123,11 @@ void HangmanLogic::joinConnection(char *text, char *category, char *language){
 
 	PlTermv term(2);
 	PlTermv stats(text, category, language);
+
 	term[0] = PlCompound("word", stats);
-	cout << (char *)term[0] << endl;
+
+	//cout << (char *)term[0] << endl;
+
 	PlQuery query("asserta", term);
 	query.next_solution();
 }
--- Social Game/MiniGames/HangMan/WCFProlog/WCFProlog/HangmanLogic.h
+++ Social Game/MiniGames/HangMan/WCFProlog/WCFProlog/HangmanLogic.h
@@ -16,6 +16,8 @@
 #include "SocialGameService.xsd.h"
 #include <vector>
 
+#define SOILPATH "..\..\..\..\..\ExternalDependencies\SOIL"
+
 using namespace std;
 
 class HangmanLogic{
@@ -23,10 +25,35 @@ public:
 	HangmanLogic();
 	~HangmanLogic();
 
-	void assertWord(char *text, char *category, char *language);
+	/*
+	Treats and then asserts word facts into the prolog engine from a Word vector
+	*/
 	void assertData(vector<Word> words);
-	void joinConnection(char *, char *, char *);
-	void chooseWord(string language, string topic);
+
+	/*
+	Assert a single word fact into the prolog engine from the provided text, category and language
+	*/
+	void joinConnection(char *text, char *category, char *language);
+
+	/*
+	Request a new random word from the topic and language in play
+	*/
+	string chooseWord(string language, string topic);
+
+	/*
+	Auxiliary method that provides easy conversion from a WCHAR* to a char*
+	*/
+	char* processWideChar(WCHAR* wc);
+
+	/*
+	Retrieve every topic from a specified language
+	*/
+	vector<string> getTopicsByLanguage(string language);
+
+	/*
+	Determine whether the game is won from the word and guesses in play
+	*/
+	bool isGameWon(string word, char* guessList);
 };
 
 #endif
