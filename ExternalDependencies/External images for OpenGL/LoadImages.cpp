9dbb838d5133228946f5aa1a95648702b1167f46
Ricardo Barbosa <1101461@isep.ipp.pt>
2014-01-09 13:22:48 +0000

    Updated LoadImages - Added loadTransparentTexture function to allow for texturing from alpha channel provided images - Added loadOpaqueTexture to provide better texuring functionality for Hangman's new UI - Updated SOIL-based functionality to provide sharper textures

--- ExternalDependencies/External images for OpenGL/LoadImages.cpp
+++ ExternalDependencies/External images for OpenGL/LoadImages.cpp
@@ -1,4 +1,4 @@
-#include <External images for OpenGL\LoadImages.h>
+#include "LoadImages.h"
 
 
 
@@ -13,6 +13,56 @@ GLuint carrega_texturas(char * nomeTextura){
 		SOIL_LOAD_AUTO,
 		SOIL_CREATE_NEW_ID,
 		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+		);
+
+
+	/* check for an error during the load process */
+	if (0 == imagem)
+	{
+		printf("SOIL loading error: '%s'\n", SOIL_last_result());
+	}
+
+	return imagem;
+}
+
+GLuint loadOpaqueTexture(char * nomeTextura){
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glEnable(GL_TEXTURE_2D);
+
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	GLuint imagem = SOIL_load_OGL_texture
+		(
+		nomeTextura,
+		SOIL_LOAD_RGB,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB
+		);
+
+
+	/* check for an error during the load process */
+	if (0 == imagem)
+	{
+		printf("SOIL loading error: '%s'\n", SOIL_last_result());
+	}
+
+	glDisable(GL_TEXTURE_2D);
+
+	return imagem;
+}
+
+GLuint loadTransparentTexture(char * nomeTextura){
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glEnable(GL_TEXTURE_2D);
+
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+	GLuint imagem = SOIL_load_OGL_texture
+		(
+		nomeTextura,
+		SOIL_LOAD_RGBA,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB
 		);
 
 
--- ExternalDependencies/External images for OpenGL/Loadimages.h
+++ ExternalDependencies/External images for OpenGL/Loadimages.h
@@ -4,8 +4,12 @@
 #include <string>
 #include <sstream>
 #include <stdlib.h>
-#include <GL/glut.h>
+#include <GLUT/GL/glut.h>
 #include <vector>
 #include <SOIL\SOIL.h>
 
 GLuint carrega_texturas(char * nomeTextura);
+
+GLuint loadOpaqueTexture(char * nomeTextura);
+
+GLuint loadTransparentTexture(char * nomeTextura);
