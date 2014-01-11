f03335d8a413b1a6427451ae4146aeefea89abbc
Márcio Martins <marciomartins.it@gmail.com>
2014-01-11 02:38:35 +0000

    Scores Update Migrations

--- Web/EpicWareWeb/EpicWareWeb/Models/ScoreUser.cs
+++ Web/EpicWareWeb/EpicWareWeb/Models/ScoreUser.cs
@@ -10,7 +10,18 @@ namespace EpicWareWeb.Models
     [DataContract]
     public class ScoreUser
     {
+        /*
+         * Mission complete -> 5 points
+         * MiniGames        -> 2 point
+         * 
+         */
         [DataMember]
         public int scoreUserID { get; set; }
+
+        [DataMember]
+        public Game MyProperty { get; set; }
+
+        [DataMember]
+        public int points { get; set; }
     }
 }
