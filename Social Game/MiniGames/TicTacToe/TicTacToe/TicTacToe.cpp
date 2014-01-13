e435bfff001d0bf25f619a2b2a4ac42ede0219d9
Ricardo Barbosa <1101461@isep.ipp.pt>
2014-01-13 19:10:37 +0000

    ref#10 Updated TicTacToe  project - Added TicTacToe and TicTacToeLogic to better modulate the project - Moved the images for better integration with the final SocialGame - Replaced GLUT library with freeGLUT for better integration with SocialGame

--- Social Game/MiniGames/TicTacToe/TicTacToe/TicTacToe.cpp
+++ Social Game/MiniGames/TicTacToe/TicTacToe/TicTacToe.cpp
@@ -1,27 +1,8 @@
-#include <iostream> 
-#include <fstream> 
-#include <stdio.h>
-#include <string>
-#include <sstream>
-#include <stdlib.h>
 #include <freeGLUT\include\GL\freeglut.h>
-#include <vector>
-//#include "SOIL.h"
 #include <External images for OpenGl\Loadimages.h>
-//#include "LoadExternalFiles.h"
-#include <conio.h>
-#include <windows.h>
-#include <iostream>
-#include <SWI-Prolog.h>
-#include <SWI-Stream.h>
-#include <SWI-cpp.h>
-//#include "Prolog" 
-
-//#pragma comment(linker, "/subsystem:\"windows\" \
-//	/entry:\"mainCRTStartup\"")
+#include "TicTacToe.h"
 
 using namespace std;
-
 #define BUFSIZE 512
 
 int teste = 0;
@@ -47,10 +28,6 @@ typedef struct{
 	GLuint deuce;
 }Modelo;
 
-typedef struct{
-	int l;
-	int c;
-} Cordenadas;
 Quadrado quad;
 Modelo mod;
 //if true PLAYER NON PC a jogar, if false PC a jogar
@@ -59,6 +36,9 @@ bool terminouJogo;
 bool repetirJogada;
 int vencedor = -1;
 
+
+TicTacToe::TicTacToe(){};
+TicTacToe::~TicTacToe(){};
 
 /* INICIALIZAÇÃO DE JOGO */
 
@@ -80,15 +60,15 @@ void initGame(){
 	resetMatriz();
 
 	/* carregar imagens */
-	mod.imagem_circle = load2D(".\/images\/tictactoe\/CIRCLE.png");
-	mod.imagem_cruz = load2D(".\/images\/tictactoe\/CRUZ.png");
-	mod.textura_paredes = load2D(".\/images\/tictactoe\/PAREDE.png");
-	mod.win = load2D(".\/images\/tictactoe\/WIN.png");
-	mod.lose = load2D(".\/images\/tictactoe\/LOSE.png");
-	mod.deuce = load2D(".\/images\/tictactoe\/DEUCE.png");
+	mod.imagem_circle = load2D("CIRCLE.png");
+	mod.imagem_cruz = load2D("CRUZ.png");
+	mod.textura_paredes = load2D("PAREDE.png");
+	mod.win = load2D("WIN.png");
+	mod.lose = load2D("LOSE.png");
+	mod.deuce = load2D("DEUCE.png");
 	for (int i = 0; i < 3; i++){
 		for (int j = 0; j < 3; j++){
-		mod.textura_quadrado[i][j] = load2D(".\/images\/tictactoe\/PAREDE.png");
+			mod.textura_quadrado[i][j] = load2D("PAREDE.png");
 		}
 	}
 
@@ -103,237 +83,9 @@ void initGame(){
 void initMenu(void)
 {
 	/*  texturas  menu */
-	mod.menu = load2D(".\/images\/tictactoe\/MENU.png");
-	mod.exit = load2D(".\/images\/tictactoe\/EXIT.png");
-	mod.play = load2D(".\/images\/tictactoe\/PLAY.png");
-}
-
-
-/* PROLOG */
-void addBase(char c)
-{
-	PlTermv bv1(1);
-	char *pointerVal1;
-	char tempArr1[10];
-	sprintf_s(tempArr1, "%c", c);
-	pointerVal1 = tempArr1;
-
-	PlTermv az1(pointerVal1);
-	bv1[0] = PlCompound("base", az1);
-	cout << (char*)bv1[0] << endl;
-	PlQuery p1("assertz", bv1);
-	p1.next_solution();
-}
-
-void addAllBase(char *arr){
-	char b0 = arr[0];
-	char b1 = arr[1];
-	char b2 = arr[2];
-	char b3 = arr[3];
-	char b4 = arr[4];
-	char b5 = arr[5];
-	char b6 = arr[6];
-	char b7 = arr[7];
-	char b8 = arr[8];
-
-	addBase(b0);
-	addBase(b1);
-	addBase(b2);
-	addBase(b3);
-	addBase(b4);
-	addBase(b5);
-	addBase(b6);
-	addBase(b7);
-	addBase(b8);
-}
-
-/*
- Transforma a matriz do jogo num array com elementos a inserir na base de conhecimento do prolog
-*/
-void assertData(){
-	char arr[9];
-	int posArray = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++){
-			if (mod.matJogo[i][j] == 1)
-			{
-				arr[posArray] = 'x';
-			}
-			else if (mod.matJogo[i][j] == 2)
-			{
-				arr[posArray] = 'o';
-			}
-			else{
-				arr[posArray] = 'N';
-			}
-			posArray++;
-		}
-		
-	}
-	addAllBase(arr);
-}
-
-Cordenadas fastestWayAvailable(){
-
-	assertData();
-
-	PlTermv av(2);
-
-	char *res = 0;
-	char *res2 = 0;
-
-	/* Obtem as cordenadas da jogado do computador*/
-	PlQuery q("c", av);
-	while (q.next_solution())
-	{
-		res = (char *)av[0];
-		res2 = (char *)av[1];
-	}
-
-	Cordenadas cord;
-	cord.l = atoi(res2) - 1;
-	cord.c = atoi(res) - 1;
-	return cord;
-}
-
-/*
-VALORES DE RETORNO (PARA JÁ NÃO UTILIZADOS, PODERÁ SER UTIL NO FUTURO)
--1 Posição de jogada ocupada
-0 Jogada válida
-1 Jogador Ganhou
-2 Jogo terminou sem vencedores
-*/
-int validaJogada(int player, int l, int c)
-{
-	//Verifica posição ocupada
-	if (mod.matJogo[l][c] != 0)
-	{
-		repetirJogada = true;
-		return -1;
-	}
-	else{
-		repetirJogada = false;
-		mod.matJogo[l][c] = player;
-
-		/* VERIFICAÇÃO SE JOGADOR GANHOU COM A JOGADA EFETUADA*/
-
-		//Verifica linha completa
-		bool flag = true;
-		for (int i = 0; i < 3; i++)
-		{
-			if (mod.matJogo[l][i] != player)
-			{
-				flag = false;
-			}
-		}
-		if (flag)
-		{
-			terminouJogo = true;
-			vencedor = player;
-			return 1;
-			
-		}
-
-
-		//Verifica coluna completa
-		flag = true;
-		for (int i = 0; i < 3; i++)
-		{
-			if (mod.matJogo[i][c] != player)
-			{
-				flag = false;
-			}
-		}
-		if (flag)
-		{
-			terminouJogo = true;
-			vencedor = player;
-			return 1;
-			
-		}
-
-
-		//Verifica diagonal 1
-		flag = true;
-		for (int i = 0; i < 3; i++)
-		{
-			if (mod.matJogo[i][i] != player)
-			{
-				flag = false;
-			}
-		}
-		if (flag)
-		{
-			terminouJogo = true;
-			vencedor = player;
-			return 1;
-			
-		}
-
-		//Verifica diagonal 2
-		flag = true;
-		if (mod.matJogo[0][2] != player)
-		{
-			flag = false;
-		}
-		if (mod.matJogo[1][1] != player)
-		{
-			flag = false;
-		}
-		if (mod.matJogo[2][0] != player)
-		{
-			flag = false;
-		}
-
-		if (flag)
-		{
-			terminouJogo = true;
-			vencedor = player;
-			return 1;
-			
-		}
-
-		/* VERIFICACA SE JOGO TERMINOU SEM VENCEDOR*/
-		bool terminou = true;
-		for (int i = 0; i < 3; i++)
-		{
-			for (int j = 0; j < 3; j++)
-			{
-				if (mod.matJogo[i][j] == 0)
-				{
-					terminou = false;
-				}
-			}
-		}
-		if (terminou)
-		{
-			terminouJogo = true;
-			vencedor = 0;
-			return 2;
-		}
-		else{
-			return 0;
-		}
-	}
-}
-
-int jogadaJogador(int l, int c)
-{
-	return validaJogada(1,l,c);
-}
-
-
-int jogadaComputador()
-{
-	if (!terminouJogo && !repetirJogada){
-		player = false;
-		Cordenadas cord;
-		cord = fastestWayAvailable();
-		player = true;
-		return validaJogada(2, cord.l, cord.c);
-	}
-	return -1;
+	mod.menu = load2D("MENU.png");
+	mod.exit = load2D("EXIT.png");
+	mod.play = load2D("PLAY.png");
 }
 
 /* DESNHOS NO ECRA */
@@ -409,13 +161,13 @@ void desenhaJogo(GLenum mode)
 		{
 			if (mod.matJogo[i][j] == 0)
 			{
-				mod.textura_quadrado[i][j] = load2D(".\/images\/tictactoe\/PAREDE.png");
+				mod.textura_quadrado[i][j] = load2D("PAREDE.png");
 			}
-			else if(mod.matJogo[i][j] == 1){
-				mod.textura_quadrado[i][j] = load2D(".\/images\/tictactoe\/CRUZ.png");
+			else if (mod.matJogo[i][j] == 1){
+				mod.textura_quadrado[i][j] = load2D("CRUZ.png");
 			}
 			else{
-				mod.textura_quadrado[i][j] = load2D(".\/images\/tictactoe\/CIRCLE.png");
+				mod.textura_quadrado[i][j] = load2D("CIRCLE.png");
 			}
 		}
 	}
@@ -453,46 +205,46 @@ void desenhaFinal(GLenum mode)
 {
 	/* carregar fundo */
 	glPushMatrix();
-		glLoadName(20);
-		glBindTexture(GL_TEXTURE_2D, mod.menu);
-		//glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, 0.5); //transparencias 
-		glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 1.0);
-		glVertex2f(0, 0);
-		glTexCoord2f(1.0, 1.0);
-		glVertex2f(100, 0);
-		glTexCoord2f(1.0, 0.0);
-		glVertex2f(100, 100);
-		glTexCoord2f(0.0, 0.0);
-		glVertex2f(0, 100);
-		glEnd();
-		glBindTexture(GL_TEXTURE_2D, NULL);
+	glLoadName(20);
+	glBindTexture(GL_TEXTURE_2D, mod.menu);
+	//glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, 0.5); //transparencias 
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0, 1.0);
+	glVertex2f(0, 0);
+	glTexCoord2f(1.0, 1.0);
+	glVertex2f(100, 0);
+	glTexCoord2f(1.0, 0.0);
+	glVertex2f(100, 100);
+	glTexCoord2f(0.0, 0.0);
+	glVertex2f(0, 100);
+	glEnd();
+	glBindTexture(GL_TEXTURE_2D, NULL);
 	glPopMatrix();
 
 	/* carrega painel  final */
 
 	glPushMatrix();
-		glLoadName(1);
-		if (vencedor == 0){
-			glBindTexture(GL_TEXTURE_2D, mod.deuce);
-		}
-		else if (vencedor == 1){
-			glBindTexture(GL_TEXTURE_2D, mod.win);
-		}
-		else{
-			glBindTexture(GL_TEXTURE_2D, mod.lose);
-		}
-		glBegin(GL_POLYGON);
-		glTexCoord2f(0.0, 1.0);
-		glVertex2f(15, 30);
-		glTexCoord2f(1.0, 1.0);
-		glVertex2f(85, 30);
-		glTexCoord2f(1.0, 0.0);
-		glVertex2f(85, 57);
-		glTexCoord2f(0.0, 0.0);
-		glVertex2f(15, 57);
-		glEnd();
-		glBindTexture(GL_TEXTURE_2D, NULL);
+	glLoadName(1);
+	if (vencedor == 0){
+		glBindTexture(GL_TEXTURE_2D, mod.deuce);
+	}
+	else if (vencedor == 1){
+		glBindTexture(GL_TEXTURE_2D, mod.win);
+	}
+	else{
+		glBindTexture(GL_TEXTURE_2D, mod.lose);
+	}
+	glBegin(GL_POLYGON);
+	glTexCoord2f(0.0, 1.0);
+	glVertex2f(15, 30);
+	glTexCoord2f(1.0, 1.0);
+	glVertex2f(85, 30);
+	glTexCoord2f(1.0, 0.0);
+	glVertex2f(85, 57);
+	glTexCoord2f(0.0, 0.0);
+	glVertex2f(15, 57);
+	glEnd();
+	glBindTexture(GL_TEXTURE_2D, NULL);
 	glPopMatrix();
 
 	/*  sair btn  */
@@ -531,7 +283,7 @@ void reshape(int w, int h)
 
 }
 
-void display(void) 
+void display(void)
 {
 	if (!terminouJogo){
 		if (teste == 0){
@@ -580,6 +332,8 @@ void clickEvent(GLint hits, GLuint buffer[])
 
 void clickEventMatrix(GLint hits, GLuint buffer[])
 {
+	TicTacToeLogic l;
+
 	if (player && !terminouJogo){
 		Cordenadas jogada;
 		int i;
@@ -593,55 +347,55 @@ void clickEventMatrix(GLint hits, GLuint buffer[])
 				if (*ptr == 1){
 					printf("1");
 					glutPostRedisplay();
-					jogadaJogador(0, 0);
+					l.jogadaJogador(0, 0);
 				}
 				else if (*ptr == 2){
 					printf("2");
 					glutPostRedisplay();
-					jogadaJogador(0, 1);
+					l.jogadaJogador(0, 1);
 				}
 				else if (*ptr == 3){
 					printf("3");
 					glutPostRedisplay();
-					jogadaJogador(0, 2);
+					l.jogadaJogador(0, 2);
 				}
 				else if (*ptr == 4){
 					printf("4");
 					glutPostRedisplay();
-					jogadaJogador(1, 0);
+					l.jogadaJogador(1, 0);
 				}
 				else if (*ptr == 5){
 					printf("5");
 					glutPostRedisplay();
-					jogadaJogador(1, 1);
+					l.jogadaJogador(1, 1);
 				}
 				else if (*ptr == 6){
 					printf("6");
 					glutPostRedisplay();
-					jogadaJogador(1, 2);
+					l.jogadaJogador(1, 2);
 				}
 				else if (*ptr == 7){
 					printf("7");
 					glutPostRedisplay();
-					jogadaJogador(2, 0);
+					l.jogadaJogador(2, 0);
 				}
 				else if (*ptr == 8){
 					printf("8");
 					glutPostRedisplay();
-					jogadaJogador(2, 1);
+					l.jogadaJogador(2, 1);
 				}
 				else if (*ptr == 9){
 					printf("9");
 					glutPostRedisplay();
-					jogadaJogador(2, 2);
+					l.jogadaJogador(2, 2);
 				}
 				ptr++;
 			}
 		}
-		jogadaComputador();
+		l.jogadaComputador();
 	}
 	else{}
-	
+
 }
 
 void pickRects(int button, int state, int x, int y)
@@ -713,14 +467,7 @@ void picking(int button, int state, int x, int y)
 	}
 }
 
-/* MAIN */
-
-int main(int argc, char** argv)
-{
-	_putenv("SWI_HOME_DIR=C:\\Program Files (x86)\\swipl");
-	char* dummy_args[] = { argv[0], "-s", ".\/pl\/tictactoe.pl", NULL };
-
-	PlEngine e(3, dummy_args);
+int TicTacToe::startGame(int argc, char** argv){
 
 	glutInit(&argc, argv);
 
@@ -738,15 +485,9 @@ int main(int argc, char** argv)
 
 	glutReshapeFunc(reshape);
 
-	glutSetOption(
-		GLUT_ACTION_ON_WINDOW_CLOSE,
-		GLUT_ACTION_GLUTMAINLOOP_RETURNS
-		);
-
 	initMenu(); //começa por carregar o menu
 
 	glutMainLoop();
 
 	return 0;
 }
-
---
+++ Social Game/MiniGames/TicTacToe/TicTacToe/TicTacToe.h
@@ -0,0 +1,10 @@
+#include "TicTacToeLogic.h"
+
+class TicTacToe
+{
+private:
+public:
+	TicTacToe::TicTacToe();
+	TicTacToe::~TicTacToe();
+	int TicTacToe::startGame(int, char **);
+};
