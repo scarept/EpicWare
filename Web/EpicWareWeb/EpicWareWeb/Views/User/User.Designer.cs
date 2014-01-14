ce3a0a74e8ba8a859cef2711cfe4f5eee49492b7
Márcio Martins <marciomartins.it@gmail.com>
2014-01-14 23:36:08 +0000

    Resolve merge conflits

--- Web/EpicWareWeb/EpicWareWeb/Views/User/User.Designer.cs
+++ Web/EpicWareWeb/EpicWareWeb/Views/User/User.Designer.cs
@@ -601,6 +601,15 @@ namespace EpicWareWeb.Views.User {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Pedido de amizade enviado pelo jogo.
+        /// </summary>
+        public static string pedidoAmizadePeloJogo {
+            get {
+                return ResourceManager.GetString("pedidoAmizadePeloJogo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Pedir Introdução.
         /// </summary>
         public static string pedir_intro {
