f3a4cf6f1325b0c1b2fdaecd8f5663d6629a1edd
Ricardo Barbosa <1101461@isep.ipp.pt>
2014-01-15 02:24:59 +0000

    ref #25 Fixed script in order to include assets

--- Social Game/BuildAndMakeMiniGames.bat
+++ Social Game/BuildAndMakeMiniGames.bat
@@ -1,7 +1,16 @@
 echo off
 
-SET SOCIAL_PATH=SocialGame\SocialGame\images
 SET BINARIES_PATH=MiniGames
+SET SOCIAL_PATH=SocialGame\SocialGame\images
+SET HANGMAN_RES_PATH=HangMan\WCFProlog\WCFProlog\images
+SET HANGMAN_RES_PATH2=HangMan\WCFProlog\WCFProlog\pl
+SET LABYRINTH_RES_PATH=Labyrinth\labyrinth\labyrinth\images
+SET LABYRINTH_RES_PATH2=Labyrinth\labyrinth\labyrinth\pl
+SET TICTACTOE_RES_PATH=TicTacToe\TicTacToe\images
+SET TICTACTOE_RES_PATH2=TicTacToe\TicTacToe\pl
+
+
+
 SET HANGMAN_PATH=HangMan\WCFProlog\Release
 SET LABYRINTH_PATH=Labyrinth\labyrinth\Release
 SET TICTACTOE_PATH=TicTacToe\Release
@@ -9,7 +18,12 @@ SET OUTPUT_PATH=SocialGame\Release
 SET OUTPUT_PATH2=SocialGame\Debug
 
 MD %OUTPUT_PATH%
+MD %OUTPUT_PATH%\images
+MD %OUTPUT_PATH%\pl
+pause
 MD %OUTPUT_PATH2%
+MD %OUTPUT_PATH2%\images
+MD %OUTPUT_PATH2%\pl
 
 xcopy /I /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%HANGMAN_PATH%" ".\%OUTPUT_PATH%"
 xcopy /I /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%HANGMAN_PATH%" ".\%OUTPUT_PATH2%"
@@ -19,4 +33,12 @@ xcopy /I /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%TICTACTOE_PATH
 xcopy /I /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%TICTACTOE_PATH%" ".\%OUTPUT_PATH2%"
 xcopy /I /Y /E /exclude:excludedfileslist2.txt ".\%SOCIAL_PATH%" ".\%OUTPUT_PATH%\images"
 xcopy /I /Y /E /exclude:excludedfileslist2.txt ".\%SOCIAL_PATH%" ".\%OUTPUT_PATH2%\images"
-pause
+
+xcopy /I /Y /E ".\%BINARIES_PATH%\%HANGMAN_RES_PATH%" ".\%OUTPUT_PATH%\images"
+xcopy /I /Y /E ".\%BINARIES_PATH%\%HANGMAN_RES_PATH2%" ".\%OUTPUT_PATH2%\pl"
+
+xcopy /I /Y /E ".\%BINARIES_PATH%\%LABYRINTH_RES_PATH%" ".\%OUTPUT_PATH%\images"
+xcopy /I /Y /E ".\%BINARIES_PATH%\%LABYRINTH_RES_PATH2%" ".\%OUTPUT_PATH2%\pl"
+
+xcopy /I /Y /E ".\%BINARIES_PATH%\%TICTACTOE_RES_PATH%" ".\%OUTPUT_PATH%\images"
+xcopy /I /Y /E ".\%BINARIES_PATH%\%TICTACTOE_RES_PATH2%" ".\%OUTPUT_PATH2%\pl"
