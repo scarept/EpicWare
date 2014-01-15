83cc155d537a00bdc1d220e93f2c500f9c7c0ef1
Ricardo Barbosa <1101461@isep.ipp.pt>
2014-01-15 04:08:56 +0000

    ref #25 Fixed mini games script not copying assets to RELEASE/DEBUG

--- Social Game/BuildAndMakeMiniGames.bat
+++ Social Game/BuildAndMakeMiniGames.bat
@@ -9,7 +9,20 @@ SET LABYRINTH_RES_PATH2=Labyrinth\labyrinth\labyrinth\pl
 SET TICTACTOE_RES_PATH=TicTacToe\TicTacToe\images
 SET TICTACTOE_RES_PATH2=TicTacToe\TicTacToe\pl
 
+SET HANGMAN_RELEASE_IMG=HangMan\WCFProlog\Release\images
+SET HANGMAN_RELEASE_PL=HangMan\WCFProlog\Release\pl
+SET HANGMAN_RELEASE2_IMG=HangMan\WCFProlog\Debug\images
+SET HANGMAN_RELEASE2_PL=HangMan\WCFProlog\Debug\pl
 
+SET LABYRINTH_RELEASE_IMG=Labyrinth\labyrinth\Release\images
+SET LABYRINTH_RELEASE_PL=Labyrinth\labyrinth\Release\pl
+SET LABYRINTH_RELEASE2_IMG=Labyrinth\labyrinth\Debug\images
+SET LABYRINTH_RELEASE2_PL=Labyrinth\labyrinth\Debug\pl
+
+SET TICTACTOE_RELEASE_IMG=TicTacToe\Release\images
+SET TICTACTOE_RELEASE_PL=TicTacToe\Release\pl
+SET TICTACTOE_RELEASE2_IMG=TicTacToe\Debug\images
+SET TICTACTOE_RELEASE2_PL=TicTacToe\Debug\pl
 
 SET HANGMAN_PATH=HangMan\WCFProlog\Release
 SET LABYRINTH_PATH=Labyrinth\labyrinth\Release
@@ -51,11 +64,27 @@ xcopy /I /Y /E ".\%BINARIES_PATH%\%TICTACTOE_RES_PATH2%" ".\%OUTPUT_PATH2%\pl"
 
 xcopy /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%HANGMAN_PATH%" ".\%OUTPUT_PATH%"
 xcopy /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%HANGMAN_PATH%" ".\%OUTPUT_PATH2%"
-
+pause
 xcopy /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%LABYRINTH_PATH%" ".\%OUTPUT_PATH%"
 xcopy /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%LABYRINTH_PATH%" ".\%OUTPUT_PATH2%"
 xcopy /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%TICTACTOE_PATH%" ".\%OUTPUT_PATH%"
 xcopy /Y /E /exclude:excludedfileslist.txt ".\%BINARIES_PATH%\%TICTACTOE_PATH%" ".\%OUTPUT_PATH2%"
 xcopy /Y /E /exclude:excludedfileslist2.txt ".\%SOCIAL_PATH%" ".\%OUTPUT_PATH%\images"
 xcopy /Y /E /exclude:excludedfileslist2.txt ".\%SOCIAL_PATH%" ".\%OUTPUT_PATH2%\images"
+pause
+xcopy /Y /E ".\%BINARIES_PATH%\%HANGMAN_RES_PATH%" ".\%BINARIES_PATH%\%HANGMAN_RELEASE_IMG%"
+xcopy /Y /E ".\%BINARIES_PATH%\%HANGMAN_RES_PATH2%" ".\%BINARIES_PATH%\%HANGMAN_RELEASE_PL%"
+xcopy /Y /E ".\%BINARIES_PATH%\%HANGMAN_RES_PATH%" ".\%BINARIES_PATH%\%HANGMAN_RELEASE2_IMG%"
+xcopy /Y /E ".\%BINARIES_PATH%\%HANGMAN_RES_PATH2%" ".\%BINARIES_PATH%\%HANGMAN_RELEASE2_PL%"
+pause
+xcopy /Y /E ".\%BINARIES_PATH%\%LABYRINTH_RES_PATH%" ".\%BINARIES_PATH%\%LABYRINTH_RELEASE_IMG%"
+xcopy /Y /E ".\%BINARIES_PATH%\%LABYRINTH_RES_PATH2%" ".\%BINARIES_PATH%\%LABYRINTH_RELEASE_PL%"
+xcopy /Y /E ".\%BINARIES_PATH%\%LABYRINTH_RES_PATH%" ".\%BINARIES_PATH%\%LABYRINTH_RELEASE2_IMG%"
+xcopy /Y /E ".\%BINARIES_PATH%\%LABYRINTH_RES_PATH2%" ".\%BINARIES_PATH%\%LABYRINTH_RELEASE2_PL%"
+pause
+xcopy /Y /E ".\%BINARIES_PATH%\%TICTACTOE_RES_PATH%" ".\%BINARIES_PATH%\%TICTACTOE_RELEASE_IMG%"
+xcopy /Y /E ".\%BINARIES_PATH%\%TICTACTOE_RES_PATH2%" ".\%BINARIES_PATH%\%TICTACTOE_RELEASE_PL%"
+xcopy /Y /E ".\%BINARIES_PATH%\%TICTACTOE_RES_PATH%" ".\%BINARIES_PATH%\%TICTACTOE_RELEASE2_IMG%"
+xcopy /Y /E ".\%BINARIES_PATH%\%TICTACTOE_RES_PATH2%" ".\%BINARIES_PATH%\%TICTACTOE_RELEASE2_PL%"
+
 pause
